0039c2674080dd52803b2f14dd59f863c5643b2c
misho6344 <mikheili.abuladze.1@btu.edu.ge>
2025-01-26 17:33:32 +0400

    commit

---
+++ numberguessing_mikheil abuladze/numberguessing/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace numberguessing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            number obj = new number();
+            obj.met1();
+        }
+    }
+}
